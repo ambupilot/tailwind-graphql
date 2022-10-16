bda4391ac4587cd4c26664a19b76aa69d01b89eb
Martijn Kerssing <martijn@kerssing.nl>
2022-10-16 20:19:56 +0200

    Feature en Hero toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Example from "./components/Example";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Feature from "./components/Feature";
+import Hero from "./components/Hero";
 
 function App() {
   return (
         <div className="md:container md:mx-auto px-4">
           <Header />
+          <Hero />
+          <Feature />
           <Example />
         </div>
   );
